35138085d2aee170b161a78116e89fcd8443e6e9
abushoaib99 <abushoaib99@gmail.com>
2024-05-30 19:11:08 +0600

    - code update

--- js/currency_mask_converter.js
+++ js/currency_mask_converter.js
@@ -49,6 +49,9 @@ function countDot(str) {
 function isCommaRemoved(str1, str2){
     let cnt1 = 0;
     let cnt2 = 0;
+    if(!str1 || !str2){
+        return;
+    }
     for(let char of str1){
         if(char === ','){
             cnt1++;
@@ -78,177 +81,186 @@ function makeCurrencyFormat(value){
     }
 }
 
-(function(){
-    let $amount1 = $('input[type=amount]');
-    $amount1.css({'text-align': 'right'})
-    $amount1.val("0.00"); // Initial value
-    let allowDigit = [...'0123456789'];
-
-    $amount1.on('click', function(e){
-        let cursorPosStart = $(this)[0].selectionStart;
-        console.log("cursorPosStart::: ", cursorPosStart);
-    });
-
-    let oriValue = null;
-    let oriValueLen = null;
-    let cursorPosStart = null;
-    let cursorPosEnd = null;
-    let cursorPos = null;
-
-    $(document).on('keyup keydown', 'input[type=amount]', function(e){
-        let pressedKey = e.key;
-        let eventType = e.type;
-        let value = $(this).val();
-        value = value.replace(/[^0-9.,]/g, '');
-        let integerValue = value.replace(/[^0-9.]/g, '').split('.')[0];
-
-        if((integerValue.length>11)
-            && allowDigit.includes(pressedKey)){
-            return false;
+function makeAmountField($amountField = null){
+    if ($amountField === null ){
+        return;
+    }
+    $amountField.each((index, element)=>{
+        let $field = $(element);
+        $field.css({'text-align': 'right'});
+        let existingValue = $field.val();
+        if (!existingValue){
+            $field.val("0.00"); // Initial value
         }
 
-        if(pressedKey === 'Delete'){
-            return false;
-        }
+        let allowDigit = [...'0123456789'];
+        let allowKey = ['Backspace', 'ArrowLeft', 'ArrowRight', '.'];
 
-        if(eventType === 'keydown'){
-            oriValue = value;
-            oriValueLen = value.length;
-            cursorPosStart = $(this)[0].selectionStart;
-            cursorPosEnd = $(this)[0].selectionEnd;
-            cursorPos = oriValueLen - cursorPosStart;
+        let oriValue = null;
+        let oriValueLen = null;
+        let cursorPosStart = null;
+        let cursorPosEnd = null;
+        let cursorPos = null;
 
-            let partialSelection = ((cursorPosEnd - cursorPosStart) > 1) && (cursorPosStart !== 0 && cursorPosEnd !== oriValueLen);
+        $field.on('keyup keydown paste', function(e){
+            e.stopImmediatePropagation();
+            let pressedKey = e.key;
+            let eventType = e.type;
+            let value = $(this).val();
+            value = value.replace(/[^0-9.,]/g, '');
+            let integerValue = value.replace(/[^0-9.]/g, '').split('.')[0];
 
-            if (partialSelection){
-                // Prevent delete partial selected value
+            if((integerValue.length>11)
+                && allowDigit.includes(pressedKey)){
                 return false;
             }
-            if(allowDigit.includes(pressedKey)){
-                // For remove cursor blink from laptop browser
+            if (eventType === 'paste'){
                 return false;
             }
+            if(eventType === 'keydown'){
+                oriValue = value;
+                oriValueLen = value.length;
+                cursorPosStart = $(this)[0].selectionStart;
+                cursorPosEnd = $(this)[0].selectionEnd;
+                cursorPos = oriValueLen - cursorPosStart;
 
-            if(cursorPosStart){
-                /// To delete previous value of comma
-                let prevCurPos = cursorPosStart-1;
-                let prevChar = value[prevCurPos];
-                if(prevChar === ','){
-                    cursorPosStart--;
-                }
-            }
-        }
-        if(eventType === 'keyup'){
-            let keyUpValue = $(this).val();
-            let keyUpValueLen = keyUpValue.length;
+                let partialSelection = ((cursorPosEnd - cursorPosStart) > 1) && (cursorPosStart !== 0 && cursorPosEnd !== oriValueLen);
 
-            let cntDot = countDot(keyUpValue);
-
-            if(cursorPos === 2 && cntDot === 0){
-                /// If `.` (dot) delete
-                let addIndex = cursorPosStart-1;
-                let removeIndex = cursorPosStart - 2;
-                // Add `.` (.) at position 2 from last digit
-                value = addCharAt(value, addIndex, '.');
-                if(value.length === 4){
-                    // replace first digit by `0`
-                    value = replaceCharAt(value, removeIndex, '0');
+                if (partialSelection){
+                    // Prevent delete partial selected value
+                    return false;
                 }
-                else{
-                    // remove first digit
-                    value = removeCharAt(value, removeIndex);
-                }
-                
-                // Update field by new updated value
-                makeCurrencyFormat.call(this, value);
-                let newCurPos = value.length - 3;
-                // Move cursor to the after `.` position
-                focusCursorPosition.call(this, newCurPos);
-                return false;
-            }
-
-            if(cntDot > 1 && oriValue !== keyUpValue){
-                // If press `.`, then cursor move to at index 2 from last
-                $(this).val(oriValue);
-                let dotPosition = oriValueLen - 2;
-                // Move cursor to the `.` position
-                focusCursorPosition.call(this, dotPosition);
-                return false;
-            }
-
-            if ([0, 1, 2].includes(cursorPos)){
-                /// Action for decimal part value (after `.`)
-                let newCurPos = cursorPosStart;
-                if(oriValueLen > keyUpValueLen){
-                    // If digit deleted then replace the previous cursor position value by `0`
-                    value = replaceCharAt(oriValue, cursorPosStart-1, '0');
-                    newCurPos = cursorPosStart - 1;
-                }
-                if(allowDigit.includes(pressedKey) && cursorPos !== 0){
-                    // Replace digit by pressed key of current place
-                    newCurPos = cursorPosStart + 1;
-                    value = replaceCharAt(value, cursorPosStart, pressedKey);
-                }
-                $(this).val(value);
-                let valueLen = value.length;
-                if((newCurPos !== cursorPosStart) || (value !== keyUpValue)){
-                    // Move cursor to the next index
-                    focusCursorPosition.call(this, newCurPos);
-                }
-                if(valueLen < 4){
-                    // If delete all digit from cursor start point
-                    let newValue = '0.00';
-                    let newCurPos = 0;
-                    if(cursorPos === 1){
-                        newValue = `0.0${value[valueLen-1]}`;
-                        newCurPos = 3;
-                    }
-                    if (cursorPos === 2){
-                        newValue = `0.${value[valueLen-2]}${value[valueLen-1]}`;
-                        newCurPos = 2;
-                    }
-
-                    $(this).val(newValue);
-                    focusCursorPosition.call(this, newCurPos);
-                }
-            }
-            else{
-                // Action for integer part value (before `.`)
                 if(allowDigit.includes(pressedKey)){
-                    if (cursorPosStart === 0 && oriValueLen === 4){
-                        /// Replace first digit by pressed digit for one integer value
-                        value = replaceCharAt(value, 0, pressedKey);
-                        cursorPosStart++;
+                    // For remove cursor blink from laptop browser
+                    return false;
+                }
+
+                if(allowKey.includes(pressedKey) === false){
+                    // Don't allow any key without `allowKey`
+                    return false;
+                }
+
+                if(cursorPosStart){
+                    /// To delete previous value of comma
+                    let prevCurPos = cursorPosStart-1;
+                    let prevChar = value[prevCurPos];
+                    if(prevChar === ','){
+                        cursorPosStart--;
+                    }
+                }
+            }
+            if(eventType === 'keyup'){
+                let keyUpValue = $(this).val();
+                let keyUpValueLen = keyUpValue.length;
+
+                let cntDot = countDot(keyUpValue);
+
+                if(cursorPos === 2 && cntDot === 0){
+                    /// If `.` (dot) delete
+                    let addIndex = cursorPosStart-1;
+                    let removeIndex = cursorPosStart - 2;
+                    // Add `.` (.) at position 2 from last digit
+                    value = addCharAt(value, addIndex, '.');
+                    if(value.length === 4){
+                        // replace first digit by `0`
+                        value = replaceCharAt(value, removeIndex, '0');
                     }
                     else{
-                        /// Add the pressed digit at the selected cursor index
-                        value = addCharAt(value, cursorPosStart, pressedKey);
+                        // remove first digit
+                        value = removeCharAt(value, removeIndex);
+                    }
+
+                    // Update field by new updated value
+                    makeCurrencyFormat.call(this, value);
+                    let newCurPos = value.length - 3;
+                    // Move cursor to the after `.` position
+                    focusCursorPosition.call(this, newCurPos);
+                    return false;
+                }
+
+                if(cntDot > 1 && oriValue !== keyUpValue){
+                    // If press `.`, then cursor move to at index 2 from last
+                    $(this).val(oriValue);
+                    let dotPosition = oriValueLen - 2;
+                    // Move cursor to the `.` position
+                    focusCursorPosition.call(this, dotPosition);
+                    return false;
+                }
+
+                if ([0, 1, 2].includes(cursorPos)){
+                    /// Action for decimal part value (after `.`)
+                    let newCurPos = cursorPosStart;
+                    if(oriValueLen > keyUpValueLen){
+                        // If digit deleted then replace the previous cursor position value by `0`
+                        value = replaceCharAt(oriValue, cursorPosStart-1, '0');
+                        newCurPos = cursorPosStart - 1;
+                    }
+                    if(allowDigit.includes(pressedKey) && cursorPos !== 0){
+                        // Replace digit by pressed key of current place
+                        newCurPos = cursorPosStart + 1;
+                        value = replaceCharAt(value, cursorPosStart, pressedKey);
+                    }
+                    $(this).val(value);
+                    let valueLen = value.length;
+                    if((newCurPos !== cursorPosStart) || (value !== keyUpValue)){
+                        // Move cursor to the next index
+                        focusCursorPosition.call(this, newCurPos);
+                    }
+                    if(valueLen < 4){
+                        // If delete all digit from cursor start point
+                        let newValue = '0.00';
+                        let newCurPos = 0;
+                        if(cursorPos === 1){
+                            newValue = `0.0${value[valueLen-1]}`;
+                            newCurPos = 3;
+                        }
+                        if (cursorPos === 2){
+                            newValue = `0.${value[valueLen-2]}${value[valueLen-1]}`;
+                            newCurPos = 2;
+                        }
+
+                        $(this).val(newValue);
+                        focusCursorPosition.call(this, newCurPos);
                     }
                 }
-                // Get comma is deleted or not
-                let isCommaRem = isCommaRemoved(oriValue, keyUpValue);
-                if(isCommaRem){
-                    // If back button pressed from comma index,
-                    // then removed the previous digit of comma
-                    value = removeCharAt(keyUpValue, cursorPosStart-1);
-                }
+                else{
+                    // Action for integer part value (before `.`)
+                    if(allowDigit.includes(pressedKey)){
+                        if (cursorPosStart === 0 && oriValueLen === 4){
+                            /// Replace first digit by pressed digit for one integer value
+                            value = replaceCharAt(value, 0, pressedKey);
+                            cursorPosStart++;
+                        }
+                        else{
+                            /// Add the pressed digit at the selected cursor index
+                            value = addCharAt(value, cursorPosStart, pressedKey);
+                        }
+                    }
+                    // Get comma is deleted or not
+                    let isCommaRem = isCommaRemoved(oriValue, keyUpValue);
+                    if(isCommaRem){
+                        // If back button pressed from comma index,
+                        // then removed the previous digit of comma
+                        value = removeCharAt(keyUpValue, cursorPosStart-1);
+                    }
 
-                /// Update the input field by new value
-                makeCurrencyFormat.call(this, value);
+                    /// Update the input field by new value
+                    makeCurrencyFormat.call(this, value);
 
-                let newValue = $(this).val();
-                
-                let newValueLen = newValue.length;
-                let addLen = newValueLen - oriValueLen;
-                cursorPosStart = cursorPosStart + addLen;
-                cursorPosStart = cursorPosStart < 0 ? 0 : cursorPosStart;
+                    let newValue = $(this).val();
 
-                if(((newValue !== oriValue) || (newValue !== keyUpValue))){
-                    /// If input field value changed then move the cursor accordingly
-                    focusCursorPosition.call(this, cursorPosStart);
+                    let newValueLen = newValue.length;
+                    let addLen = newValueLen - oriValueLen;
+                    cursorPosStart = cursorPosStart + addLen;
+                    cursorPosStart = cursorPosStart < 0 ? 0 : cursorPosStart;
+
+                    if(((newValue !== oriValue) || (newValue !== keyUpValue))){
+                        /// If input field value changed then move the cursor accordingly
+                        focusCursorPosition.call(this, cursorPosStart);
+                    }
                 }
             }
-        }
+        })
     })
-}());
+}
 
